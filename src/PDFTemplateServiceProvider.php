dba634e310889e223705b4fafc82e5829018ca27
pphatdev <pphat.kits@gmail.com>
2025-05-12 15:00:09 +0700

    Refactor PDFTemplateServiceProvider: simplify register method and update view publishing paths

--- src/PDFTemplateServiceProvider.php
+++ src/PDFTemplateServiceProvider.php
@@ -11,13 +11,7 @@ class PDFTemplateServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/pdf-template.php',
-            'pdf-template'
-        );
-    }
+    public function register() {}
 
     /**
      * Bootstrap services.
@@ -26,22 +20,12 @@ class PDFTemplateServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // Publish configuration
-        $this->publishes([
-            __DIR__ . '/../config/pdf-template.php' => config_path('pdf-template.php'),
-        ], 'config');
-
         // Publish views
         $this->publishes([
-            __DIR__ . '/../src/views' => resource_path('views/vendor/pdf-template'),
+            __DIR__ . '/../views/example/' => resource_path('views/pdf-template'),
         ], 'views');
 
-        // Publish fonts
-        $this->publishes([
-            __DIR__ . '/../fonts' => public_path('fonts'),
-        ], 'fonts');
-
         // Load views
-        $this->loadViewsFrom(__DIR__ . '/../src/views', 'pdf-template');
+        // $this->loadViewsFrom(__DIR__ . '/../src/views/ex', 'pdf-template');
     }
 }
