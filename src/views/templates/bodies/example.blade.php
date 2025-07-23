620476cc87c2a6f8304e3b3c12b850a5f1201b3e
pphatdev <pphat.kits@gmail.com>
2025-07-23 11:20:27 +0700

    ✨ feat: Add example table template for PDF generation

---
+++ src/views/templates/bodies/example.blade.php
@@ -0,0 +1,18 @@
+
+<table style="width:100%; margin-left: 0; margin-right: 0; margin-top: 0rem; margin-bottom: 1rem; border: thin solid white; border-collapse: collapse; font-family: 'ttstandinvoice';">
+
+    <tr>
+        @for ($i = 0; $i < $cols; $i++)
+            <th style="border: thin solid white; hieght: 30px; padding: 15px; background-color: #1fa8e0"></th>
+        @endfor
+    </tr>
+
+    @for ($i = 0; $i < $rows; $i++)
+        <tr>
+            @for ($j = 0; $j < $cols; $j++)
+                <td style="border: thin solid white; padding: 12px; background-color: {{ $i % 2 == 0 ? '#82dcff' : '#c2eeff' }}"></td>
+            @endfor
+        </tr>
+    @endfor
+
+</table>
